ced491afb86e6465b06cbd35258da280d121b978
ITITIU20340 <91869154+ITITIU20340@users.noreply.github.com>
2021-10-06 08:27:37 +0700

    Update Exercise2.c

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
     int Minvalue = testcase[0];
 	int Maxvalue = testcase[0];
 	for (int i = 0; i < argc; i++){
-		if(Minxvalue >= testcase[i]){
+		if(Minvalue >= testcase[i]){
 			Minvalue = testcase[i];
 		}
 		if(Maxvalue <= testcase[i]){
